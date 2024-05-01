544aa1d60adceda54999973ce294fd883abe91a3
Orlando Porta <orlando_12368@hotmail.com>
2024-04-30 19:28:39 -0500

    page task.jsx and Formtask.jsx v1.0.2

--- client/pages/Task/Task.jsx
+++ client/pages/Task/Task.jsx
@@ -1,26 +1,43 @@
-import { useState } from 'react';
-import { Grid, Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import { useState } from "react";
+import {
+  Grid,
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+  Box,
+} from "@mui/material";
+import TaskForm from "../../components/form/FormTask";
+import { IMG_3, IMG_4 } from "../../assets/img/images";
 
 export default function MediaCard() {
   const [showCreateTaskInput, setShowCreateTaskInput] = useState(false);
-  const [text, setText] = useState('');
 
   const handleCreateClick = () => {
     setShowCreateTaskInput(true);
   };
 
+  const handleSaveTask = (data) => {
+    console.log("Datos de la tarea:", data);
+    setShowCreateTaskInput(false);
+  };
+
   return (
-    <Grid container spacing={2} justifyContent="center">
+    <Grid container spacing={2} justifyContent="center" mt={10}>
       <Grid item>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            sx={{ height: 140 }}
-            image="/static/images/cards/contemplative-reptile.jpg"
-            title="Crear Tarea"
-          />
+        <Card sx={{ maxWidth: 300, boxShadow: "0px 8px 8px -4px rgba(0, 0, 0, 0.5)" }}>
           <CardContent>
+            <Box borderBottom={8} borderColor="transparent">
+              <Box borderTopLeftRadius={4} borderTopRightRadius={4} overflow="hidden" borderRadius={3}>
+                <img
+                  src={IMG_3}
+                  width={300}
+                  height={150}
+                  alt="Imagen 3"
+                />
+              </Box>
+            </Box>
             <Typography gutterBottom variant="h5" component="div">
               Crear Nueva Tarea
             </Typography>
@@ -29,37 +46,49 @@ export default function MediaCard() {
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small" onClick={handleCreateClick}>Agregar Tarea</Button>
+            <Button size="small" onClick={handleCreateClick}>
+              Agregar Tarea
+            </Button>
           </CardActions>
         </Card>
       </Grid>
       <Grid item>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            sx={{ height: 140 }}
-            image="/static/images/cards/contemplative-reptile.jpg"
-            title="Editar y/o Eliminar Tareas"
-          />
+        <Card sx={{ maxWidth: 300, boxShadow: "0px 8px 8px -4px rgba(0, 0, 0, 0.5)" }}>
           <CardContent>
+            <Box borderBottom={8} borderColor="transparent">
+              <Box borderTopLeftRadius={4} borderTopRightRadius={4} overflow="hidden" borderRadius={3}>
+                <img
+                  src={IMG_4}
+                  width={300}
+                  height={150}
+                  alt="Imagen 4"
+                />
+              </Box>
+            </Box>
             <Typography gutterBottom variant="h5" component="div">
-              Editar y/o Eliminar Tareas
+              Editar o eliminar tarea
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Selecciona una tarea para editar su información o eliminarla.
+              Puedes editar o eliminar tarea dándole clic al botón correspondiente.
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small">Editar</Button>
-            <Button size="small" color="error">Eliminar</Button>
+            <Button size="small" onClick={handleCreateClick}>
+              Editar tarea
+            </Button>
+            <Button size="small" color="error" onClick={handleCreateClick}>
+              Eliminar tarea
+            </Button>
           </CardActions>
         </Card>
       </Grid>
 
       {showCreateTaskInput && (
-        <Grid item xs={12} style={{ marginTop: '20px' }}>
-          <ReactQuill value={text} onChange={setText} />
+        <Grid item xs={12} style={{ marginTop: "20px" }}>
+          <TaskForm onSave={handleSaveTask} />
         </Grid>
       )}
     </Grid>
   );
 }
+
